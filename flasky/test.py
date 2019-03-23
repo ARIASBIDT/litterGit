2e08ee47dffd462c0ad3df0a08d2a8c439476af8
cat <864915038@qq.com>
2019-03-23 09:15:28 +0800

    check

--- flasky/test.py
+++ flasky/test.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from app.main.predict import predict
+import json
+from flask import request
 app = Flask(__name__)
-@app.route('/')
+@app.route('/',methods=['POST'])
 def index():
-    return '<h1>Hello world!</h>'
+    val = json.loads(request.files('file'))
+    return '<h1>{}</h1>'.format(val)
+    # l = predict(test_path='.\\test', model_path='.\\app\\model\\mobilenet_my_finetune_model_ep15_d3_lr0.0001.h5')
+    # l_j = json.dumps(l)
+    # return l_j
+# print(json.loads(l_j))
+#     #return '<h1>Hello world!</h>'
 if __name__== "__main__":
     app.run(host='0.0.0.0',port=5000)
